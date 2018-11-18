37c24db60616ccd314263f4bb4495b021c0c02d5
sina_ <sina_@DESKTOP-7L1N12C.eduroam.hs-furtwangen.de>
2018-11-18 15:52:42 +0100

    html geändert

--- Aufgabe4/WbK.js
+++ Aufgabe4/WbK.js
@@ -1,1 +1,171 @@
+var Weihnachtsbaumkonfigurator;
+(function (Weihnachtsbaumkonfigurator) {
+    document.addEventListener("DOMContentLoaded", init);
+    document.addEventListener("DOMContentLoaded", init2);
+    document.addEventListener("DOMContentLoaded", init3);
+    document.addEventListener("DOMContentLoaded", init4);
+    function init() {
+        let fieldsets = document.getElementsByTagName("fieldset");
+        for (let i = 0; i < 1; i++) {
+            let fieldset = fieldsets[i];
+            fieldset.addEventListener("change", handleChange);
+            //document.getElementById("summary").addEventListener("click", summary);
+            //BAUMART
+            let node = document.getElementById("Baumart");
+            let childNodeHtml;
+            let art = Weihnachtsbaumkonfigurator.tree;
+            childNodeHtml = "";
+            childNodeHtml += "<select name=Select id=select>";
+            childNodeHtml += "<optgroup label=Baumarten></optgroup> ";
+            childNodeHtml += "<datalist id=options>";
+            for (let i = 0; i < Weihnachtsbaumkonfigurator.tree.length; i++) {
+                childNodeHtml += "<option value=";
+                childNodeHtml += "lam" + i;
+                childNodeHtml += ">";
+                childNodeHtml += Weihnachtsbaumkonfigurator.tree[i].name;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "</hr>";
+            node.innerHTML += childNodeHtml;
+        }
+        //LAMETTA    
+        let node = document.getElementById("Lametta");
+        let childNodeHtml;
+        let art = Weihnachtsbaumkonfigurator.tinsel;
+        childNodeHtml = "";
+        childNodeHtml += "<select name=Select id=select>";
+        childNodeHtml += "<optgroup label=Lametta></optgroup> ";
+        childNodeHtml += "<datalist id=options>";
+        for (let i = 0; i < Weihnachtsbaumkonfigurator.tinsel.length; i++) {
+            childNodeHtml += "<option value=";
+            childNodeHtml += "lam" + i;
+            childNodeHtml += ">";
+            childNodeHtml += Weihnachtsbaumkonfigurator.tinsel[i].name;
+            childNodeHtml += "</option>";
+        }
+        childNodeHtml += "</select>";
+        childNodeHtml += "<select>";
+        childNodeHtml += "<h3>Anzahl:</h3>";
+        childNodeHtml += "<option value=clear selected></option>";
+        for (let i = 0; i < 20; i += 5) {
+            childNodeHtml += "<option value=lamettanum";
+            childNodeHtml += i;
+            childNodeHtml += " id=lamettanum";
+            childNodeHtml += i;
+            childNodeHtml += ">";
+            childNodeHtml += i;
+            childNodeHtml += "</option>";
+        }
+        childNodeHtml += "</select>";
+        childNodeHtml += "</hr>";
+        node.innerHTML += childNodeHtml;
+    }
+    //KERZEN
+    function init2() {
+        let fieldsets = document.getElementsByTagName("fieldset");
+        for (let i = 0; i < 1; i++) {
+            let fieldset = fieldsets[i];
+            let node = document.getElementById("Kerzen");
+            let childNodeHtml;
+            let art = Weihnachtsbaumkonfigurator.candle;
+            childNodeHtml = "";
+            childNodeHtml += "<select name=Select id=select>";
+            childNodeHtml += "<optgroup label=Kerzen></optgroup> ";
+            childNodeHtml += "<datalist id=options>";
+            for (let i = 0; i < Weihnachtsbaumkonfigurator.candle.length; i++) {
+                childNodeHtml += "<option value=";
+                childNodeHtml += "lam" + i;
+                childNodeHtml += ">";
+                childNodeHtml += Weihnachtsbaumkonfigurator.candle[i].name;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "<select>";
+            childNodeHtml += "<h3>Anzahl:</h3>";
+            childNodeHtml += "<option value=clear selected></option>";
+            for (let i = 0; i < 20; i += 1) {
+                childNodeHtml += "<option value=candlenum";
+                childNodeHtml += i;
+                childNodeHtml += " id=candlenum";
+                childNodeHtml += i;
+                childNodeHtml += ">";
+                childNodeHtml += i;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "</hr>";
+            node.innerHTML += childNodeHtml;
+        }
+    }
+    //KUGELN
+    function init3() {
+        let fieldsets = document.getElementsByTagName("fieldset");
+        for (let i = 0; i < 1; i++) {
+            let fieldset = fieldsets[i];
+            let node = document.getElementById("Kugeln");
+            let childNodeHtml;
+            let art = Weihnachtsbaumkonfigurator.ball;
+            childNodeHtml = "";
+            childNodeHtml += "<select name=Select id=select>";
+            childNodeHtml += "<optgroup label=Kugeln></optgroup> ";
+            childNodeHtml += "<datalist id=options>";
+            for (let i = 0; i < Weihnachtsbaumkonfigurator.candle.length; i++) {
+                childNodeHtml += "<option value=";
+                childNodeHtml += "lam" + i;
+                childNodeHtml += ">";
+                childNodeHtml += Weihnachtsbaumkonfigurator.ball[i].name;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "<select>";
+            childNodeHtml += "<h3>Anzahl:</h3>";
+            childNodeHtml += "<option value=clear selected></option>";
+            for (let i = 0; i < 20; i += 1) {
+                childNodeHtml += "<option value=ballnum";
+                childNodeHtml += i;
+                childNodeHtml += " id=ballnum";
+                childNodeHtml += i;
+                childNodeHtml += ">";
+                childNodeHtml += i;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "</hr>";
+            node.innerHTML += childNodeHtml;
+        }
+    }
+    //Versand
+    function init4() {
+        let fieldsets = document.getElementsByTagName("fieldset");
+        for (let i = 0; i < 1; i++) {
+            let fieldset = fieldsets[i];
+            let node = document.getElementById("Versand");
+            let childNodeHtml;
+            let art = Weihnachtsbaumkonfigurator.shipment;
+            childNodeHtml = "";
+            childNodeHtml += "<select name=Select id=select>";
+            childNodeHtml += "<optgroup label=Versand></optgroup> ";
+            childNodeHtml += "<datalist id=options>";
+            for (let i = 0; i < Weihnachtsbaumkonfigurator.shipment.length; i++) {
+                childNodeHtml += "<option value=";
+                childNodeHtml += "lam" + i;
+                childNodeHtml += ">";
+                childNodeHtml += Weihnachtsbaumkonfigurator.shipment[i].name;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "</hr>";
+            node.innerHTML += childNodeHtml;
+        }
+    }
+    function handleChange(_event) {
+        console.log(_event);
+        let target = _event.target;
+        console.log("Changed " + target.name + " to " + target.value);
+        if (this.id == "Lametta")
+            console.log("Changed " + target.name + " to " + target.checked);
+        let checkbox = document.getElementById("summary");
+    }
+})(Weihnachtsbaumkonfigurator || (Weihnachtsbaumkonfigurator = {}));
 //# sourceMappingURL=WbK.js.map
--- Aufgabe4/WbK.ts
+++ Aufgabe4/WbK.ts
@@ -1,6 +1,5 @@
 namespace Weihnachtsbaumkonfigurator {
-
-     document.addEventListener("DOMContentLoaded", init);
+    document.addEventListener("DOMContentLoaded", init);
     document.addEventListener("DOMContentLoaded", init2);
     document.addEventListener("DOMContentLoaded", init3);
     document.addEventListener("DOMContentLoaded", init4);
@@ -10,7 +9,7 @@ namespace Weihnachtsbaumkonfigurator {
             let fieldset: HTMLFieldSetElement = fieldsets[i];
             fieldset.addEventListener("change", handleChange);
             //document.getElementById("summary").addEventListener("click", summary);
-        
+
             //BAUMART
             let node: HTMLElement = document.getElementById("Baumart");
             let childNodeHtml: string;
@@ -29,12 +28,12 @@ namespace Weihnachtsbaumkonfigurator {
                 childNodeHtml += "</option>";
             }
             childNodeHtml += "</select>";
-         
+
             childNodeHtml += "</hr>";
 
             node.innerHTML += childNodeHtml;
-    }
-//LAMETTA    
+        }
+        //LAMETTA    
         let node: HTMLElement = document.getElementById("Lametta");
         let childNodeHtml: string;
         let art: Product[] = tinsel;
@@ -69,218 +68,133 @@ namespace Weihnachtsbaumkonfigurator {
 
         node.innerHTML += childNodeHtml;
     }
-//KERZEN
+    //KERZEN
     function init2(): void {
         let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
         for (let i: number = 0; i < 1; i++) {
             let fieldset: HTMLFieldSetElement = fieldsets[i];
-    
-    let node: HTMLElement = document.getElementById("Kerzen");
-    let childNodeHtml: string;
-    let art: Product[] = candle;
 
-    childNodeHtml = "";
-    childNodeHtml += "<select name=Select id=select>";
-    childNodeHtml += "<optgroup label=Kerzen></optgroup> ";
-    childNodeHtml += "<datalist id=options>";
+            let node: HTMLElement = document.getElementById("Kerzen");
+            let childNodeHtml: string;
+            let art: Product[] = candle;
 
-    for (let i: number = 0; i < candle.length; i++) {
-        childNodeHtml += "<option value=";
-        childNodeHtml += "lam" + i;
-        childNodeHtml += ">";
-        childNodeHtml += candle[i].name;
-        childNodeHtml += "</option>";
-    }
-    childNodeHtml += "</select>";
-    childNodeHtml += "<select>";
-    childNodeHtml += "<h3>Anzahl:</h3>";
-    childNodeHtml += "<option value=clear selected></option>";
-    for (let i: number = 0; i < 20; i += 1) {
-        childNodeHtml += "<option value=candlenum";
-        childNodeHtml += i;
-        childNodeHtml += " id=candlenum";
-        childNodeHtml += i;
-        childNodeHtml += ">";
-        childNodeHtml += i;
-        childNodeHtml += "</option>";
-    }
-    childNodeHtml += "</select>";
-    childNodeHtml += "</hr>";
+            childNodeHtml = "";
+            childNodeHtml += "<select name=Select id=select>";
+            childNodeHtml += "<optgroup label=Kerzen></optgroup> ";
+            childNodeHtml += "<datalist id=options>";
 
-    node.innerHTML += childNodeHtml;
+            for (let i: number = 0; i < candle.length; i++) {
+                childNodeHtml += "<option value=";
+                childNodeHtml += "lam" + i;
+                childNodeHtml += ">";
+                childNodeHtml += candle[i].name;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "<select>";
+            childNodeHtml += "<h3>Anzahl:</h3>";
+            childNodeHtml += "<option value=clear selected></option>";
+            for (let i: number = 0; i < 20; i += 1) {
+                childNodeHtml += "<option value=candlenum";
+                childNodeHtml += i;
+                childNodeHtml += " id=candlenum";
+                childNodeHtml += i;
+                childNodeHtml += ">";
+                childNodeHtml += i;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "</hr>";
+
+            node.innerHTML += childNodeHtml;
         }
     }
-    
-  //KUGELN
+
+    //KUGELN
     function init3(): void {
         let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
         for (let i: number = 0; i < 1; i++) {
             let fieldset: HTMLFieldSetElement = fieldsets[i];
-    
-    let node: HTMLElement = document.getElementById("Kugeln");
-    let childNodeHtml: string;
-    let art: Product[] = ball;
 
-    childNodeHtml = "";
-    childNodeHtml += "<select name=Select id=select>";
-    childNodeHtml += "<optgroup label=Kugeln></optgroup> ";
-    childNodeHtml += "<datalist id=options>";
+            let node: HTMLElement = document.getElementById("Kugeln");
+            let childNodeHtml: string;
+            let art: Product[] = ball;
 
-    for (let i: number = 0; i < candle.length; i++) {
-        childNodeHtml += "<option value=";
-        childNodeHtml += "lam" + i;
-        childNodeHtml += ">";
-        childNodeHtml += ball[i].name;
-        childNodeHtml += "</option>";
-    }
-    childNodeHtml += "</select>";
-    childNodeHtml += "<select>";
-    childNodeHtml += "<h3>Anzahl:</h3>";
-    childNodeHtml += "<option value=clear selected></option>";
-    for (let i: number = 0; i < 20; i += 1) {
-        childNodeHtml += "<option value=ballnum";
-        childNodeHtml += i;
-        childNodeHtml += " id=ballnum";
-        childNodeHtml += i;
-        childNodeHtml += ">";
-        childNodeHtml += i;
-        childNodeHtml += "</option>";
-    }
-    childNodeHtml += "</select>";
-    childNodeHtml += "</hr>";
+            childNodeHtml = "";
+            childNodeHtml += "<select name=Select id=select>";
+            childNodeHtml += "<optgroup label=Kugeln></optgroup> ";
+            childNodeHtml += "<datalist id=options>";
 
-    node.innerHTML += childNodeHtml;
+            for (let i: number = 0; i < candle.length; i++) {
+                childNodeHtml += "<option value=";
+                childNodeHtml += "lam" + i;
+                childNodeHtml += ">";
+                childNodeHtml += ball[i].name;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "<select>";
+            childNodeHtml += "<h3>Anzahl:</h3>";
+            childNodeHtml += "<option value=clear selected></option>";
+            for (let i: number = 0; i < 20; i += 1) {
+                childNodeHtml += "<option value=ballnum";
+                childNodeHtml += i;
+                childNodeHtml += " id=ballnum";
+                childNodeHtml += i;
+                childNodeHtml += ">";
+                childNodeHtml += i;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+            childNodeHtml += "</hr>";
+
+            node.innerHTML += childNodeHtml;
         }
     }
-  //Versand
+    //Versand
     function init4(): void {
         let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
         for (let i: number = 0; i < 1; i++) {
             let fieldset: HTMLFieldSetElement = fieldsets[i];
-    
-    let node: HTMLElement = document.getElementById("Versand");
-    let childNodeHtml: string;
-    let art: Product[] = shipment;
 
-    childNodeHtml = "";
-    childNodeHtml += "<select name=Select id=select>";
-    childNodeHtml += "<optgroup label=Versand></optgroup> ";
-    childNodeHtml += "<datalist id=options>";
+            let node: HTMLElement = document.getElementById("Versand");
+            let childNodeHtml: string;
+            let art: Product[] = shipment;
 
-    for (let i: number = 0; i < shipment.length; i++) {
-        childNodeHtml += "<option value=";
-        childNodeHtml += "lam" + i;
-        childNodeHtml += ">";
-        childNodeHtml += shipment[i].name;
-        childNodeHtml += "</option>";
+            childNodeHtml = "";
+            childNodeHtml += "<select name=Select id=select>";
+            childNodeHtml += "<optgroup label=Versand></optgroup> ";
+            childNodeHtml += "<datalist id=options>";
+
+            for (let i: number = 0; i < shipment.length; i++) {
+                childNodeHtml += "<option value=";
+                childNodeHtml += "lam" + i;
+                childNodeHtml += ">";
+                childNodeHtml += shipment[i].name;
+                childNodeHtml += "</option>";
+            }
+            childNodeHtml += "</select>";
+
+            childNodeHtml += "</hr>";
+
+            node.innerHTML += childNodeHtml;
+        }
     }
-    childNodeHtml += "</select>";
-    
-    childNodeHtml += "</hr>";
 
-    node.innerHTML += childNodeHtml;
-        }
-   
+    function handleChange(_event: Event): void {
+        console.log(_event);
 
+        let target: HTMLInputElement = <HTMLInputElement>_event.target;
+        console.log("Changed " + target.name + " to " + target.value);
 
-        function handleChange(_event: Event): void {
-            console.log(_event);
 
-            let target: HTMLInputElement = <HTMLInputElement>_event.target;
-            console.log("Changed " + target.name + " to " + target.value);
+        if (this.id == "Lametta")
+            console.log("Changed " + target.name + " to " + target.checked);
 
-
-            if (this.id == "Lametta")
-                console.log("Changed " + target.name + " to " + target.checked);
-            
-            let checkbox: HTMLElement= document.getElementById("summary");
-            
-            
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        let checkbox: HTMLElement = document.getElementById("summary");
 
 
 
     }
+
+}
