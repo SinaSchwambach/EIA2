9b9608a6d045f05364f92c3259ce1dda12f5224d
sina_ <sina_@DESKTOP-7L1N12C.eduroam.hs-furtwangen.de>
2018-11-18 14:37:36 +0100

    weiter gearbeitet

--- Aufgabe4/WbK.ts
+++ Aufgabe4/WbK.ts
@@ -26,109 +26,163 @@ namespace Weihnachtsbaumkonfigurator {
             childNodeHtml += "</hr>";
 
             node.innerHTML += childNodeHtml;
+
+            let node1: HTMLElement = document.getElementById("Lametta");
+            let childNodeHtml1: string;
+            let art1: Product[] = tinsel;
+
+            childNodeHtml1 = "";
+
+            for (let i: number = 0; i < art1.length; i++) {
+                childNodeHtml1 += "<input type=checkbox name=art1[i].color value=art1[i].color id=art1[i].color/>";
+                childNodeHtml1 += "<label for=check1>art1[i].color</label>";
+            }
+
+            node1.innerHTML += childNodeHtml1;
+
+            let node2: HTMLElement = document.getElementById("Kugeln");
+            let childNodeHtml2: string;
+            let art2: Product[] = ball;
+
+            childNodeHtml2 = "";
+
+            for (let i: number = 0; i < art2.length; i++) {
+                childNodeHtml2 += "<input type=checkbox name=art2[i].color value=art2[i].color id=art2[i].color/>";
+                childNodeHtml2 += "<label for=check1>art2[i].color art2[i].size</label>";
+            }
+
+            node2.innerHTML += childNodeHtml2;
+
+            let node3: HTMLElement = document.getElementById("Kerzen");
+            let childNodeHtml3: string;
+            let art3: Product[] = candle;
+
+            childNodeHtml3 = "";
+
+            for (let i: number = 0; i < art3.length; i++) {
+                childNodeHtml3 += "<input type=checkbox name=art3[i].color value=art3[i].color id=art3[i].color/>";
+                childNodeHtml3 += "<label for=check1>art3[i].color</label>";
+            }
+
+            node3.innerHTML += childNodeHtml3;
+
+            let node4: HTMLElement = document.getElementById("Versand");
+            let childNodeHtml4: string;
+            let art4: Product[] = tree;
+
+            childNodeHtml4 = "";
+            childNodeHtml4 += "<select name=Select id=select>";
+            childNodeHtml4 += "<optgroup label=Versandoptionen></optgroup> ";
+            childNodeHtml4 += "<datalist id=options>";
+
+            for (let i: number = 0; i < art4.length; i++) {
+                childNodeHtml += "<option value=option1>art4[i].name</option>";
+            }
+
+            childNodeHtml4 += "</select>";
+            childNodeHtml4 += "</hr>";
+
+            node4.innerHTML += childNodeHtml4;
+
+
         }
-        let node: HTMLElement = document.getElementById("Lametta");
-        let childNodeHtml: string;
-        let art: Product[] = tinsel;
 
-        childNodeHtml = "";
 
-        for (let i: number = 0; i < art.length; i++) {
-            childNodeHtml += "<input type=checkbox name=art[i].color value=art[i].color id=art[i].color/>";
-            childNodeHtml += "<label for=check1>art[i].color</label>";
+
+        function handleChange(_event: Event): void {
+            console.log(_event);
+
+            let target: HTMLInputElement = <HTMLInputElement>_event.target;
+            console.log("Changed " + target.name + " to " + target.value);
+
+
+            if (this.id == "Lametta")
+                console.log("Changed " + target.name + " to " + target.checked);
+
         }
 
-        node.innerHTML += childNodeHtml;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
-
-
-
-    function handleChange(_event: Event): void {
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
