482905a55853f1e8f7dabb47f5c2a60e37a6f202
sina_ <sina_@DESKTOP-7L1N12C.hfu-open.fu.hs-furtwangen.de>
2018-11-29 11:26:47 +0100

    node.d.ts hochgekaden

--- Aufgabe6/Server.ts
+++ Aufgabe6/Server.ts
@@ -7,21 +7,21 @@ namespace L06_SendData {
         port = 8100;
 
     let server: Http.Server = Http.createServer();  //wandelt Computer in Http Server
-    server.addListener("request", handleRequest);// wenn im Server eine Anfrage stattfindet dann wird die Funktion handleRequest aufgerufen
-    server.addListener("listening", handleListen);// wenn dem Sever der Listener listening übergeben wird dann wird funktion handleListen aufgerufen
-    server.listen(port);// erstellt einen Listener an Port 8100
+    server.addListener("request", handleRequest); // wenn im Server eine Anfrage stattfindet dann wird die Funktion handleRequest aufgerufen
+    server.addListener("listening", handleListen); // wenn dem Sever der Listener listening übergeben wird dann wird funktion handleListen aufgerufen
+    server.listen(port); // erstellt einen Listener an Port 8100
 
     function handleListen(): void {
-        console.log("Listening");//auf Konsole wird Listening ausgegeben
+        console.log("Listening"); //auf Konsole wird Listening ausgegeben
     }
 
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { //Variablen _request und _response werden übergeben
-        console.log("I hear voices!");//auf Konsole wird "I hear voices!" ausgegeben
+        console.log("I hear voices!"); //auf Konsole wird "I hear voices!" ausgegeben
 
-        _response.setHeader("content-type", "text/html; charset=utf-8");//Der Antwort des Servers werden Eigenschaften zugewiesen; Von welchem Typ der Inhalt sein soll und was das charset ist
-        _response.setHeader("Access-Control-Allow-Origin", "*");//Erlaubt den Zugriff auf Daten von einer anderen Quelle
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //Der Antwort des Servers werden Eigenschaften zugewiesen; Von welchem Typ der Inhalt sein soll und was das charset ist
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //Erlaubt den Zugriff auf Daten von einer anderen Quelle
 
-        _response.write(_request.url);//setzt die Änderungen an die ursprüngliche url
+        _response.write(_request.url); //setzt die Änderungen an die ursprüngliche url
         
         _response.end();//beendet die Antwort des Servers
     }
