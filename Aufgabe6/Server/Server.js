e3958fd1cbbb2aafd936f4447896de0d8ee58f83
sina_ <sina_@DESKTOP-7L1N12C>
2018-12-04 16:24:38 +0100

    package.json bearbeitet

--- Aufgabe6/Server/Server.js
+++ Aufgabe6/Server/Server.js
@@ -1,21 +1,22 @@
 "use strict";
-const Http = require("http");
+const Http = require("http"); //http Objekt wird erstellt -> interpreter schaut nach jedem m�glichen import im http Modul und f�gt es dem http Objekt im Code hinzu
 var L06_SendData;
 (function (L06_SendData) {
     console.log("Starting server"); //Konsolenausgabe Starting Server
-    let port = process.env.PORT; //Variable die den Port festlegt
+    let port = process.env.PORT; //Variable port vom typ number wird festgelegt, port gibt an an welchem Port der Server "h�ren" soll, globale process.env Variable wird von "Node" beim Start der benutzten Applikation indiziert , sie representiert den Zustand der system Umgebung deiner Applikation 
+    // wenn sie gestartet wird.
     if (port == undefined)
         port = 8100;
-    let server = Http.createServer(); //wandelt Computer in Http Server
+    let server = Http.createServer(); //variable server wird erstellt vom typ http,kann ports auf computer abrufen und funktionen ausf�hren, wandelt verhalten von computer in das eine http-servers um
     server.addListener("request", handleRequest); // wenn im Server eine Anfrage stattfindet dann wird die Funktion handleRequest aufgerufen
     server.addListener("listening", handleListen); // wenn dem Sever der Listener listening �bergeben wird dann wird funktion handleListen aufgerufen
-    server.listen(port); // erstellt einen Listener an Port 8100
+    server.listen(port); // Server sucht an angegebenem port
     function handleListen() {
         console.log("Listening"); //auf Konsole wird Listening ausgegeben
     }
     function handleRequest(_request, _response) {
         console.log("I hear voices!"); //auf Konsole wird "I hear voices!" ausgegeben
-        _response.setHeader("content-type", "text/html; charset=utf-8"); //Der Antwort des Servers werden Eigenschaften zugewiesen; Von welchem Typ der Inhalt sein soll und was das charset ist
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //im header werden die eigenschaften der antwort festgelegt (Name,wert)
         _response.setHeader("Access-Control-Allow-Origin", "*"); //Erlaubt den Zugriff auf Daten von einer anderen Quelle
         _response.write(_request.url); //setzt die �nderungen an die urspr�ngliche url
         console.log(_request.url);
--- Aufgabe6/Server/Server.ts
+++ Aufgabe6/Server/Server.ts
@@ -1,24 +1,25 @@
-import * as Http from "http";
+import * as Http from "http"; //http Objekt wird erstellt -> interpreter schaut nach jedem möglichen import im http Modul und fügt es dem http Objekt im Code hinzu
 
 namespace L06_SendData {
     console.log("Starting server"); //Konsolenausgabe Starting Server
-    let port: number = process.env.PORT; //Variable die den Port festlegt
+    let port: number = process.env.PORT; //Variable port vom typ number wird festgelegt, port gibt an an welchem Port der Server "hören" soll, globale process.env Variable wird von "Node" beim Start der benutzten Applikation indiziert , sie representiert den Zustand der system Umgebung deiner Applikation 
+               // wenn sie gestartet wird.
     if (port == undefined)  //wenn kein spezielle Port angegeben ist dann Port 8100 nutzen
         port = 8100;
 
-    let server: Http.Server = Http.createServer();  //wandelt Computer in Http Server
+    let server: Http.Server = Http.createServer();  //variable server wird erstellt vom typ http,kann ports auf computer abrufen und funktionen ausführen, wandelt verhalten von computer in das eine http-servers um
     server.addListener("request", handleRequest); // wenn im Server eine Anfrage stattfindet dann wird die Funktion handleRequest aufgerufen
     server.addListener("listening", handleListen); // wenn dem Sever der Listener listening übergeben wird dann wird funktion handleListen aufgerufen
-    server.listen(port); // erstellt einen Listener an Port 8100
+    server.listen(port); // Server sucht an angegebenem port
 
     function handleListen(): void {
         console.log("Listening"); //auf Konsole wird Listening ausgegeben
     }
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { //Variablen _request und _response werden übergeben
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { //Variablen _request und _response werden übergeben, request bezieht sich auf die anfrage des clienten und respons ist die antwort des servers
         console.log("I hear voices!"); //auf Konsole wird "I hear voices!" ausgegeben
 
-        _response.setHeader("content-type", "text/html; charset=utf-8"); //Der Antwort des Servers werden Eigenschaften zugewiesen; Von welchem Typ der Inhalt sein soll und was das charset ist
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //im header werden die eigenschaften der antwort festgelegt (Name,wert)
         _response.setHeader("Access-Control-Allow-Origin", "*"); //Erlaubt den Zugriff auf Daten von einer anderen Quelle
 
         _response.write(_request.url); //setzt die Änderungen an die ursprüngliche url
